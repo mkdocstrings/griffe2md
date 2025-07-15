56a1f31ef47548f33f09414f38f02fc647db5c18
Timothée Mazzucotelli <dev@pawamoy.fr>
2025-07-15 16:11:47 +0200

    fixup! feat: Read config from config file or pyproject.toml

--- src/griffe2md/config.py
+++ src/griffe2md/config.py
@@ -1,45 +1,48 @@
 """Load configuration."""
 
-import logging
-import pathlib
-from typing import Optional
+from __future__ import annotations
 
-import tomllib
+import logging
+import sys
+from pathlib import Path
+from typing import Any
+
+# YORE: EOL 3.10: Replace block with line 2.
+if sys.version_info >= (3, 11):
+    import tomllib
+else:
+    import tomli as tomllib
 
 logger = logging.getLogger(__name__)
 
-PATHS = (
-    ".config/griffe2md.toml",
-    "config/griffe2md.toml",
+CONFIG_FILE_PATHS = (
+    Path(".config/griffe2md.toml"),
+    Path("config/griffe2md.toml"),
+    Path("pyproject.toml"),
 )
 
-PYPROJECT = "pyproject.toml"
 
-
-def _locate_config_file() -> Optional[pathlib.Path]:
-    for path in PATHS:
-        path_ = pathlib.Path(path)
-        if path_.is_file():
-            return path_
-
-    pyproj_path = pathlib.Path(PYPROJECT)
-    if pyproj_path.is_file():
-        return pyproj_path
-
+def _locate_config_file() -> Path | None:
+    for path in CONFIG_FILE_PATHS:
+        if path.is_file():
+            return path
     return None
 
 
-def load_config() -> Optional[dict]:
-    """Load the configuration if config file or config entry in pyproject.toml exists. If neither config file was found or pyproject.toml file doesn't have [tool.griffe2md] section, returns None."""
-    config_path = _locate_config_file()
-    if config_path is None:
+def load_config() -> dict[str, Any] | None:
+    """Load the configuration if config file or config entry in pyproject.toml exists.
+
+    If neither config file was found or pyproject.toml file doesn't have
+    a `[tool.griffe2md]` section, None is returned.
+    """
+    if not (config_path := _locate_config_file()):
         return None
 
     logger.debug("Loading config from %s", config_path)
 
-    with open(config_path, "rb") as f:
+    with config_path.open("rb") as f:
         config = tomllib.load(f)
 
-    if str(config_path) == PYPROJECT:
-        return (config.get("tool") or {}).get("griffe2md")
+    if config_path.name == "pyproject.toml":
+        return config.get("tool", {}).get("griffe2md", None)
     return config
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,30 +1,24 @@
 """Test config loading."""
+
 from pathlib import Path
 from unittest.mock import Mock, patch
 
 import pytest
 
 import griffe2md.cli
-from griffe2md.config import PYPROJECT
+from griffe2md.config import CONFIG_FILE_PATHS
 
 
-@pytest.mark.parametrize(
-    "rel_path",
-    [
-        ".config/griffe2md.toml",
-        "config/griffe2md.toml",
-        "pyproject.toml",
-    ],
-)
-def test_load_config(tmpdir, rel_path: str)->None: # noqa: ANN001
+@pytest.mark.parametrize("rel_path", CONFIG_FILE_PATHS)
+def test_load_config(tmpdir: Path, rel_path: Path) -> None:
     """Test that config is loaded."""
     expected_config = {"dummy": True}
     config_text = "dummy=true"
 
     mock_write = Mock()
 
-    with tmpdir.as_cwd(), patch("griffe2md.cli.write_package_docs", mock_write):
-        text = f"[tool.griffe2md]\n{config_text}" if rel_path == PYPROJECT else config_text
+    with tmpdir.as_cwd(), patch("griffe2md.cli.write_package_docs", mock_write):  # type: ignore[attr-defined]
+        text = f"[tool.griffe2md]\n{config_text}" if rel_path.name == "pyproject.toml" else config_text
         config_path = Path(tmpdir) / rel_path
         config_path.parent.mkdir(parents=True, exist_ok=True)
         config_path.write_text(text, "utf-8")
