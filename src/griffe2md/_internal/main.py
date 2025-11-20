859c3c66984c48f2fe5c3c39909527ef60079486
Timothée Mazzucotelli <dev@pawamoy.fr>
2025-11-20 17:28:24 +0100

    fix: Don't try using the mdformat tables extension

--- src/griffe2md/_internal/main.py
+++ src/griffe2md/_internal/main.py
@@ -140,7 +140,6 @@ def render_object_docs(obj: Object, config: ConfigDict | None = None, *, format_
         rendered = mdformat.text(
             rendered,
             options={"number": "yes", "wrap": "no"},
-            extensions=("tables",),
         )
     return rendered
 
